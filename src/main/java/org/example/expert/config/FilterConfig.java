00036c4ddd6a98fed659ba268d18d03b581a1e4f
안동환 <ddh1713flrwls1713@gmail.com>
2024-10-09 13:33:09 +0900

    lv 2-9 [add/fix/del] Spring Security

--- src/main/java/org/example/expert/config/FilterConfig.java
+++ src/main/java/org/example/expert/config/FilterConfig.java
@@ -12,9 +12,9 @@ public class FilterConfig {
     private final JwtUtil jwtUtil;
 
     @Bean
-    public FilterRegistrationBean<JwtFilter> jwtFilter() {
-        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new JwtFilter(jwtUtil));
+    public FilterRegistrationBean<JwtSecurityFilter> jwtFilter() {
+        FilterRegistrationBean<JwtSecurityFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtSecurityFilter(jwtUtil));
         registrationBean.addUrlPatterns("/*"); // 필터를 적용할 URL 패턴을 지정합니다.
 
         return registrationBean;
